91b6bc653d2b26069900bf90cd56d166efa5948f
Ernesto Bossi <bossi.ernestog@gmail.com>
2014-01-26 18:03:58 -0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ templates_dir = os.path.join(PATH, ".")
 templates_files = [os.path.join(templates_dir, file) for file in os.listdir(templates_dir)]
 
 setup(name='Onyx',
-      version='0.0.1',
+      version='0.0.1b',
       description='General purpose scrapper for the web',
       author='Ernesto Bossi',
       author_email='bossi.ernestog@gmail.com',
